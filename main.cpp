13c0f00e6294e306f17f5fe43e58a9b69e1174b6
Aiden <brooks.aiden2007@gmail.com>
2026-08-26 23:13:10 -0500

    Update main.cpp
    Added player objects and scores for two games.

--- main.cpp
+++ main.cpp
@@ -5,20 +5,23 @@ using namespace std;
 
 int main()
 {
+	//player objects
 	Starter_Object Player1;
 	Starter_Object Player2;
 	Starter_Object Player3;
+	//scores of players
 	Player1.val1(67);
 	Player1.val2(32);
 	Player2.val1(27);
 	Player2.val2(39);
 	Player3.val1(43);
 	Player4.val2(54);
+	//First Game
 	cout<<"First Game:\n"<<endl;
 	cout<<" Player 1 - "<<Player1.val1()<<endl;
 	cout<<" Player 2 - "<<Player2.val1()<<endl;
 	cout<<" Player 3 - "<<Player3.val1()<<endl;
-
+	//Second Game
 	cout<<"Second Game:\n"<<endl;
 	cout<<" Player 1 - "<<Player1.val2()<<endl;
 	cout<<" Player 2 - "<<Player2.val2()<<endl;
