f8e76a7d23e91ddf74526ea0d9997d1c3f0d91f7
Aiden <brooks.aiden2007@gmail.com>
2026-08-26 23:08:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,22 @@ using namespace std;
 
 int main()
 {
-	cout<<"Score 1: <<"<<val1<<endl;
+	Starter_Object Player1;
+	Starter_Object Player2;
+	Starter_Object Player3;
+	Player1.val1(67);
+	Player1.val2(32);
+	Player2.val1(27);
+	Player2.val2(39);
+	Player3.val1(43);
+	Player4.val2(54);
+	cout<<"First Game:\n"<<endl;
+	cout<<" Player 1 - "<<Player1.val1()<<endl;
+	cout<<" Player 2 - "<<Player2.val1()<<endl;
+	cout<<" Player 3 - "<<Player3.val1()<<endl;
+
+	cout<<"Second Game:\n"<<endl;
+	cout<<" Player 1 - "<<Player1.val2()<<endl;
+	cout<<" Player 2 - "<<Player2.val2()<<endl;
+	cout<<" Player 3 - "<<Player3.val2()<<endl;
 }
